d8980673d51e1b6796dc535bd3eed7d06552ec4b
Jesper Reenberg <jesper.reenberg@gmail.com>
2015-03-23 18:25:55 +0100

    Bulk cleanup and feature improvement.

--- send2urlnamed.py
+++ send2urlnamed.py
@@ -8,12 +8,8 @@ This program will add an transfrom based on send2url.
 
 # Builtin modules
 import os
-import sys
 import zipfile
 import tempfile
-import shutil
-import platform
-import glob
 from string import Template
 import argparse
 
@@ -22,23 +18,28 @@ from lxml import etree
 from termcolor import colored
 
 
-DISCLAIMER = "This program is tested on  Maltego carbon and Maltego Chlorine."
+DISCLAIMER = """
 
+This program is tested on Maltego carbon (v3.5) and Maltego Chlorine (v3.6).
 
-DOT_TRANSFORMSETTINGS = """
-<TransformSettings enabled="true" disclaimerAccepted="true" showHelp="false">
-    <Properties>
-        <Property name="url" type="string" popup="false">$link</Property>
-        <Property name="maltego.transform.java-class-name" type="string" popup="false">
-            com.paterva.maltego.webtx.WebTxTransform
-        </Property>
-    </Properties>
-</TransformSettings>
 """
 
 
+"""The .transform represent the base definition of the transform, including
+default values.  The base definition includes defining the TransformAdapter,
+which is the Java class that is invoked when running the transform.
+
+Here the JavaTransformAdapter will run the Java class given in the
+'maltego.transform.java-class-name' property.
+
+In theory we could probably have made some cleverness with abstract and
+template, if we needed to define lots of these transforms, however this would
+make this tool overly complicated and it doesn't really matter that much that we
+generate a bunch of files.
+
+"""
 DOT_TRANSFORM = """
-<MaltegoTransform name="maltego.transform.$name" displayName="$name" abstract="false" template="false"
+<MaltegoTransform name="$nameprefix.transform.$name" displayName="$displayname" abstract="false" template="false"
                   visibility="public" description="$desc" author="$author" requireDisplayInfo="false">
     <TransformAdapter>
         com.paterva.maltego.transform.runner.test.JavaTransformAdapter
@@ -66,6 +67,30 @@ DOT_TRANSFORM = """
 """
 
 
+"""The .transformsetting represent the settings that the user may change inside
+the Maltego GUI, and these will overwrite any settings defined in the .transform
+file.  This is where we set the java class that should be executed and any
+specific values of the properties that it may reference.
+
+The WebTxTransform class references an 'url' property which it will POST all
+selected elements of the current graph to.  The url should return another url,
+which Maltego then opens in the users default browser.  If a valid url is not
+returned, Maltego will write a warning to the log window containing the returned
+text.
+
+"""
+DOT_TRANSFORMSETTINGS = """
+<TransformSettings enabled="true" disclaimerAccepted="true" showHelp="false">
+    <Properties>
+        <Property name="url" type="string" popup="false">$url</Property>
+        <Property name="maltego.transform.java-class-name" type="string" popup="false">
+            com.paterva.maltego.webtx.WebTxTransform
+        </Property>
+    </Properties>
+</TransformSettings>
+"""
+
+
 LOCALTAS = """
 <MaltegoServer name="Local" enabled="true" description="Local transforms hosted on this machine"
                url="http://localhost">
@@ -78,47 +103,41 @@ LOCALTAS = """
 """
 
 
-def writetransforms(name, link, desc, author, path):
-    """Write the .transform and .transformsettings template for this new send2url
-    transform.
+def writetransforms(path, values):
+    """Write the .transform and .transformsettings template for this new
+    'send2url' transform.
 
     """
+    tname = "%s.transform.%s" % (values.get('nameprefix'), values.get('name'))
 
-    out = Template(DOT_TRANSFORMSETTINGS).safe_substitute({'name':name, 'link':link})
-    with open(os.path.join(path, "maltego.transform." + name + ".transformsettings"), "wb") as f:
-        f.write(out)
+    with open(os.path.join(path, "%s.transformsettings" % tname), "wb") as f:
+        f.write(Template(DOT_TRANSFORMSETTINGS).safe_substitute(values))
 
-    out = Template(DOT_TRANSFORM).safe_substitute({'name':name, 'link':link, 'desc':desc, 'author':author})
-    with open(os.path.join(path, "maltego.transform." + name + ".transform"), "wb") as f:
-        f.write(out)
-
-
-
-def insert_transform(e, name):
-    """use this if actions is defined
-
-    """
+    with open(os.path.join(path, "%s.transform" % tname), "wb") as f:
+        f.write(Template(DOT_TRANSFORM).safe_substitute(values))
 
 
 
-
-def updateTAS(path, name, link):
+def updatetas(path, values):
     """Update the TAS file.
 
     Before transforms are shown inside Maltego, they need to be referenced by a
     transform application server.  In this case a Local "server".
 
     """
-    dom = etree.parse(path)
-    actions = dom.findall("./Transforms")
+    tname = "%s.transform.%s" % (values.get('nameprefix'), values.get('name'))
+    # http://stackoverflow.com/questions/7903759/pretty-print-in-lxml-is-failing-when-i-add-tags-to-a-parsed-tree
+    parser = etree.XMLParser(remove_blank_text=True)
+    dom = etree.parse(path, parser)
+    transforms = dom.findall("./Transforms")
 
-    # Insert the reference to the transform.
-    name = "maltego.transform." + name
-    etree.SubElement(actions[0], "Transform", {"name": name})
+    # Insert a <Transform> element with name set to the name of the transform we
+    # are adding.
+    etree.SubElement(transforms[0], "Transform", {"name": tname})
 
-    res = etree.tostring(dom)
-    with open(path, "wb") as fil:
-        fil.write(res)
+    res = etree.tostring(dom, pretty_print=True, encoding='utf8')
+    with open(path, "wb") as f:
+        f.write(res)
 
 
 def zip(in_path, file):
@@ -127,7 +146,7 @@ def zip(in_path, file):
     """
     with zipfile.ZipFile(file, mode="w") as zfile:
         # Add files to the zip with write.
-        for root, dirs, files in os.walk(in_path):
+        for root, _, files in os.walk(in_path):
             for file in files:
                 filepath = os.path.join(root, file)
                 # We need to store the file relative to the in_path folder,
@@ -144,23 +163,96 @@ def unzip(file, out_path):
         zfile.extractall(path=out_path)
 
 
-def main(args):
-    print colored(DISCLAIMER,"red")
+# Taken from canari.commands.common
+def parse_str(question, default):
+    """Parse a string from the user.
 
-    infile = args.mtz
+    If None is supplied, then default is returned.
+
+    """
+    return raw_input('%s [%s]: ' % (question, default)) or default
+
+# Taken from canari.commands.common
+def parse_bool(question, default=True):
+    """Parse a Boolean from the user.
+
+    The Boolean is parsed in the form of y/n or yes/no, with either Y or N as
+    the default when the user just hits enter.
+
+    """
+    choices = 'Y/n' if default else 'y/N'
+    default = 'Y' if default else 'N'
+    while True:
+        answer = raw_input('%s [%s]: ' % (question, choices)).upper() or default
+        if answer.startswith('Y'):
+            return True
+        elif answer.startswith('N'):
+            return False
+        else:
+            print("Invalid selection: '%s'. Must be either [y]es or [n]o."
+                  % answer)
+
+def ask_user(values):
+    """Ask the user about information to put into the templates.
+
+    """
+
+    # This code looks ugly and nasty, deal with it.  Keep asking the user until
+    # she is happy with the result.
+    while True:
+        while not values.get('displayname', '').strip():
+            values['displayname'] = parse_str("Display name", \
+                values.get('displayname', '')).strip()
+        # Generate a 'friendly' name from the display name
+        values['defaultname'] = values['displayname'].lower().replace(' ', '-')
+        values['name'] = parse_str("Name", \
+            values.get('name', values.get('defaultname'))).strip().lower()
+
+        # If the user didn't specify a name, use the default generated from
+        # displayname.
+        if not values['name']:
+            values['name'] = values['defaultname']
+
+        while not values.get('nameprefix', '').strip():
+            values['nameprefix'] = parse_str("Name prefix", \
+                values.get('nameprefix', 'send2url')).strip().lower()
+        print("Transform name: %s.transform.%s" %
+              (values['nameprefix'], values['name']))
+        while not values.get('url', '').strip():
+            values['url'] = parse_str("URL", values.get('url', ''))
+        values['desc'] = parse_str("Description", values.get('desc', ''))
+        values['author'] = parse_str("Author", values.get('author', ''))
+        # Keep looping if user aren't happy with the supplied info
+        if parse_bool("Are you satisfied with this information? ", True):
+            print '' # Make pretty newline...
+            break
+
+def main(args):
+    """Generates send2url transforms according to the given arguments.
+
+    """
+    print colored(DISCLAIMER, "red")
+
+
+    # Sanity check. If we don't have an input file and an output file, then it
+    # gets a bit hard to update the input file with the newly created mtz
+    # content.
+    if not args.mtz and not args.out:
+        print ("%s You must specify either an input file, an output file, or both." %
+               colored("[Error]:", "red", None, ['bold']))
+        exit(1)
 
     # Create temporary directory to store the intermediate files.
     tmp_dir = tempfile.mkdtemp()
 
-    # The .mtz file is actually just a zip file. Unpack it so we may add our
-    # send2url transforms to it.
-    unzip(infile, tmp_dir)
+    if args.mtz:
+        # The .mtz file is actually just a zip file. Unpack it so we may add our
+        # send2url transforms to it.
+        unzip(args.mtz, tmp_dir)
 
     # Ask the user for values to insert into the send2url templates.
-    author  = raw_input("Author        : ")
-    name = raw_input("Name          : ")
-    link = raw_input("Link          : ")
-    desc = raw_input("Description   : ")
+    values = {}
+    ask_user(values)
 
     repo_local_dir = os.path.join(tmp_dir, "TransformRepositories", "Local")
     servers_dir = os.path.join(tmp_dir, "Servers")
@@ -171,7 +263,7 @@ def main(args):
         os.makedirs(repo_local_dir)
 
     # Write the two templates describing the send2url local transform.
-    writetransforms(name, link, desc, author, repo_local_dir)
+    writetransforms(repo_local_dir, values)
 
     # By default local transform (seeds) are stored in 'Local.tas'.  If this
     # file doesn't exist, then write a default one.
@@ -181,26 +273,38 @@ def main(args):
         with open(servers_local_tas, "wb") as f:
             f.write(LOCALTAS)
 
+    # Add a reference to the newly created transforms inside the 'Local.tas'
+    # file.
+    updatetas(servers_local_tas, values)
 
-    updateTAS(servers_local_tas, name, link)
-
-    if args.out:
+    if not args.out:
+        print ("Input file '%s' was updated with the new transform %s." %
+               (args.mtz, values.get('displayname')))
+        zip(tmp_dir, args.mtz)
+    elif args.out == '-':
+        # TODO: Implement this
+        print colored("Not implemented. Can't write resulting zip file to "
+                      "stdout.", 'red')
+    elif args.out:
+        print ("The new transform %s was merged with input file and written "
+               "to '%s'." % (values.get('displayname'), args.out))
         zip(tmp_dir, args.out)
-        print colored("File is done...", "blue")
 
 
 
 if __name__ == '__main__':
-    description = """
+    DESCRIPTION = """
     Generate send2url local-transforms that may be imported into maltego.
 
     Will unzip the given .mtz file into a temporary location, make the needed
     changes, and write the result back into the given out file.
 
     """
-    PARSER = argparse.ArgumentParser(description=description)
-    PARSER.add_argument('mtz', help='The input .mtz file.')
+    PARSER = argparse.ArgumentParser(description=DESCRIPTION)
+    PARSER.add_argument('mtz', nargs='?',
+                        help='The input .mtz file.')
     PARSER.add_argument('-o', '--out', metavar="file",
-                        help='The output file.  If not specified, then the input file is updated.')
+                        help='The output file.  If not specified, then the ' \
+                             'input file is updated.')
 
     main(PARSER.parse_args())
