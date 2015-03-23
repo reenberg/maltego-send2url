0f78c7d3e16ff066999ad217083b0241dd78be6d
Jesper Reenberg <jesper.reenberg@gmail.com>
2015-03-23 01:09:54 +0100

    Clean up and minor modifications

--- send2urlnamed.py
+++ send2urlnamed.py
@@ -8,7 +8,6 @@ This program will add an transfrom based on send2url.
 
 # Builtin modules
 import os
-import os.path
 import sys
 import zipfile
 import tempfile
@@ -16,15 +15,17 @@ import shutil
 import platform
 import glob
 from string import Template
+import argparse
 
 # 3rd party modules.
 from lxml import etree
 from termcolor import colored
 
 
-disclaimer = "This program is tested on  Maltego carbon and Maltego Chlorine."
+DISCLAIMER = "This program is tested on  Maltego carbon and Maltego Chlorine."
 
-template1 = """
+
+DOT_TRANSFORMSETTINGS = """
 <TransformSettings enabled="true" disclaimerAccepted="true" showHelp="false">
     <Properties>
         <Property name="url" type="string" popup="false">$link</Property>
@@ -35,7 +36,8 @@ template1 = """
 </TransformSettings>
 """
 
-template2 = """
+
+DOT_TRANSFORM = """
 <MaltegoTransform name="maltego.transform.$name" displayName="$name" abstract="false" template="false"
                   visibility="public" description="$desc" author="$author" requireDisplayInfo="false">
     <TransformAdapter>
@@ -63,7 +65,8 @@ template2 = """
 </MaltegoTransform>
 """
 
-localtas = """
+
+LOCALTAS = """
 <MaltegoServer name="Local" enabled="true" description="Local transforms hosted on this machine"
                url="http://localhost">
     <LastSync>2015-01-01 00:00:00.000 CEST</LastSync>
@@ -74,106 +77,130 @@ localtas = """
 </MaltegoServer>
 """
 
-def writetransforms(name,link,desc,author,filename):
-    """ Lets write the templates """
-    out = Template(template1).safe_substitute({'name':name, 'link':link})
-    with open(filename + "maltego.transform." + name + ".transformsettings","wb") as f:
+
+def writetransforms(name, link, desc, author, path):
+    """Write the .transform and .transformsettings template for this new send2url
+    transform.
+
+    """
+
+    out = Template(DOT_TRANSFORMSETTINGS).safe_substitute({'name':name, 'link':link})
+    with open(os.path.join(path, "maltego.transform." + name + ".transformsettings"), "wb") as f:
         f.write(out)
 
-    out = Template(template2).safe_substitute({'name':name, 'link':link, 'desc':desc, 'author':author})
-    with open(filename + "maltego.transform." + name + ".transform","wb") as f:
+    out = Template(DOT_TRANSFORM).safe_substitute({'name':name, 'link':link, 'desc':desc, 'author':author})
+    with open(os.path.join(path, "maltego.transform." + name + ".transform"), "wb") as f:
         f.write(out)
-
-
-# unzip a file
-def unzip(path, directory):
-    """
-        unzip all files
-    """
-    zfile = zipfile.ZipFile(path)
-    name = ""
-    for name in zfile.namelist():
-        (dirname, filename) = os.path.split(name)
-
-        # directory
-        if not os.path.exists(directory + "/" + dirname):
-            #print "mkdir: " + directory + "/" + dirname
-            os.makedirs(directory + "/" + dirname)
-
-        if filename != '':
-            with open(directory + "/" + name, 'w') as filehd:
-                filehd.write(zfile.read(name))
-
-    zfile.close()
 
 
 
 def insert_transform(e, name):
+    """use this if actions is defined
+
     """
-        use this if actions is defined
-    """
-    #e = etree.SubElement(e, "Transforms")
-    name = "maltego.transform." + name
-    d = {"name":name}
-    e1 = etree.SubElement(e, "Transform", d)
-    return e
 
 
 
 
-def doxml(path, name, link):
-    """
-        write the new stuff to the xml file
+def updateTAS(path, name, link):
+    """Update the TAS file.
+
+    Before transforms are shown inside Maltego, they need to be referenced by a
+    transform application server.  In this case a Local "server".
+
     """
     dom = etree.parse(path)
     actions = dom.findall("./Transforms")
-    insert_transform(actions[0], name)
+
+    # Insert the reference to the transform.
+    name = "maltego.transform." + name
+    etree.SubElement(actions[0], "Transform", {"name": name})
+
     res = etree.tostring(dom)
     with open(path, "wb") as fil:
         fil.write(res)
 
 
-def zipit(directory):
+def zip(in_path, file):
+    """Add all files and directories inside the given directory to a ZipFile.
+
     """
-        Zip all the file into one mtz file
-        @todo rewrite to python
+    with zipfile.ZipFile(file, mode="w") as zfile:
+        # Add files to the zip with write.
+        for root, dirs, files in os.walk(in_path):
+            for file in files:
+                filepath = os.path.join(root, file)
+                # We need to store the file relative to the in_path folder,
+                # inside the zipfile.
+                arcname = os.path.relpath(filepath, in_path)
+                zfile.write(filepath, arcname=arcname)
+
+
+def unzip(file, out_path):
+    """Unzip all files of a given ZipFile into the specified directory.
+
     """
-    cmd = "cd %s; zip -r test.mtz * 1>/dev/null" % directory
-    os.system(cmd)
-    print colored("File is done " + directory + "/test.mtz", "blue")
+    with zipfile.ZipFile(file) as zfile:
+        zfile.extractall(path=out_path)
 
 
-def copyfile(fil):
-    """
-        Copy the file to a save location
-    """
-    shutil.copy(fil, "/tmp/" + fil.split("/")[-1])
-    return "/tmp/" + fil.split("/")[-1]
+def main(args):
+    print colored(DISCLAIMER,"red")
 
+    infile = args.mtz
 
+    # Create temporary directory to store the intermediate files.
+    tmp_dir = tempfile.mkdtemp()
 
+    # The .mtz file is actually just a zip file. Unpack it so we may add our
+    # send2url transforms to it.
+    unzip(infile, tmp_dir)
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print colored("Usage: send2urlnamed [file.mtz]","red")
-        exit()
-    print colored(disclaimer,"red")
-    infile = sys.argv[1]
-    #nfile = copyfile("test.mtz")
-    directory = tempfile.mkdtemp()
-    unzip(infile, directory)
-    #print_entities(directory)
+    # Ask the user for values to insert into the send2url templates.
     author  = raw_input("Author        : ")
     name = raw_input("Name          : ")
     link = raw_input("Link          : ")
     desc = raw_input("Description   : ")
-    if not os.path.exists(directory + "/TransformRepositories/Local/"):
-        os.mkdir(directory + "/TransformRepositories/Local/")
-    writetransforms(name,link,desc,author,directory + "/TransformRepositories/Local/")
-    #lets check if the local seed file exists.
 
-    if not os.path.exists(directory + "/Servers/Local.tas"):
-        with open(directory + "/Servers/Local.tas","wb") as f:
-            f.write(localtas)
-    doxml(directory + "/Servers/Local.tas", name, link)
-    zipit(directory)
+    repo_local_dir = os.path.join(tmp_dir, "TransformRepositories", "Local")
+    servers_dir = os.path.join(tmp_dir, "Servers")
+    servers_local_tas = os.path.join(servers_dir, "Local.tas")
+
+    # Make sure the directory exists, before we try to write files into it.
+    if not os.path.exists(repo_local_dir):
+        os.makedirs(repo_local_dir)
+
+    # Write the two templates describing the send2url local transform.
+    writetransforms(name, link, desc, author, repo_local_dir)
+
+    # By default local transform (seeds) are stored in 'Local.tas'.  If this
+    # file doesn't exist, then write a default one.
+    if not os.path.exists(servers_dir):
+        os.makedirs(servers_dir)
+    if not os.path.exists(servers_local_tas):
+        with open(servers_local_tas, "wb") as f:
+            f.write(LOCALTAS)
+
+
+    updateTAS(servers_local_tas, name, link)
+
+    if args.out:
+        zip(tmp_dir, args.out)
+        print colored("File is done...", "blue")
+
+
+
+if __name__ == '__main__':
+    description = """
+    Generate send2url local-transforms that may be imported into maltego.
+
+    Will unzip the given .mtz file into a temporary location, make the needed
+    changes, and write the result back into the given out file.
+
+    """
+    PARSER = argparse.ArgumentParser(description=description)
+    PARSER.add_argument('mtz', help='The input .mtz file.')
+    PARSER.add_argument('-o', '--out', metavar="file",
+                        help='The output file.  If not specified, then the input file is updated.')
+
+    main(PARSER.parse_args())
